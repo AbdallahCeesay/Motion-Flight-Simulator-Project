71188bdc05a89366b84073eac2279770050261d3
AbdallahCeesay <abdallah.ceesay@students.plymouth.ac.uk>
2025-02-25 16:10:34 +0000

    added control for second actuator
    Added a second actuator to be controlled

--- mbed programs/Actuator Test/main.cpp
+++ mbed programs/Actuator Test/main.cpp
@@ -6,8 +6,10 @@
 DigitalOut led(LED1);
 DigitalOut led2(LED2);
 
-PwmOut RPWM(PC_8); 
-PwmOut LPWM(PC_9);
+PwmOut act1_RPWM(PC_8); 
+PwmOut act1_LPWM(PC_9);
+PwmOut act2_RPWM(PE_5);
+PwmOut act2_LPWM(PE_6);
 
 int main()
 {
@@ -20,27 +22,43 @@ int main()
 
         if (c == 'e') {
             led = !led;
-            LPWM.write(1.0f); // extend actuator
-            RPWM.write(0);
+            act1_LPWM.write(1.0f); // extend actuator
+            act1_RPWM.write(0);
+
+            act2_LPWM.write(1.0f);
+            act2_RPWM.write(0);
         }
 
         if (c == 'q') {
             led2 = !led2;
-            RPWM.write(1.0f); // retract actuator
-            LPWM.write(0);
+            act1_RPWM.write(1.0f); // retract actuator
+            act1_LPWM.write(0);
+
+            act2_RPWM.write(1.0f);
+            act2_LPWM.write(0);
         }
 
         if (c == 's') {
-            RPWM.write(0); 
-            LPWM.write(0);
+            act1_RPWM.write(0); 
+            act1_LPWM.write(0);
+
+            act2_RPWM.write(0);
+            act2_LPWM.write(0);
         }
 
-        float rpwm_output = RPWM.read();
-        float lpwm_output = LPWM.read();
+        float act1_rpwm_output = act1_RPWM.read();
+        float act1_lpwm_output = act1_LPWM.read();
+
+        float act2_rpwm_output = act2_RPWM.read();
+        float act2_lpwm_output = act2_LPWM.read();
 
         // for debugging
-        std::cout << "rpwm_output: " << rpwm_output << endl;
-        std::cout << "lpwm_output: " << lpwm_output << endl << endl;
+        std::cout << "act1_RPWM: " << act1_rpwm_output << endl;
+        std::cout << "act1_LPWM: " << act1_lpwm_output << endl << endl;
+
+        std::cout << "act2_RPWM: " << act2_rpwm_output << endl;
+        std::cout << "act2_LPWM: " << act2_lpwm_output << endl << endl;
+
 
         //wait_us(100000);
     }
