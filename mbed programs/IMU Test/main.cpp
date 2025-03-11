8cfa5017be713133c2c4412956d0d48a90a1795f
AbdallahCeesay <abdallah.ceesay@students.plymouth.ac.uk>
2025-03-11 11:52:25 +0000

    no progress lol

--- mbed programs/IMU Test/main.cpp
+++ mbed programs/IMU Test/main.cpp
@@ -6,26 +6,26 @@ I2C i2c(PB_9, PB_8);
 
 int main()
 {
-    char data[2]; // Buffer to store the read data (2 bytes in this example)
-    int BNO085_ADDR = 0x4A; // 7-bit I2C address
-    int numberOfByteToRead = 2; // Number of bytes to read
+    char data[2]; 
+    int BNO085_ADDR = 0x4A; 
+    int numberOfBytesToRead = 2;
+    i2c.frequency(1000000);
 
     while (true) {
-        // Perform I2C read operation
-        int i2c_flag = i2c.read(BNO085_ADDR, data, numberOfByteToRead, 1);
+
+        int i2c_flag = i2c.read(BNO085_ADDR, data, numberOfBytesToRead, 1);
 
         // Check if the read operation was successful
         if (i2c_flag == 0) {
             std::cout << "IMU Detected" << std::endl;
 
-            // Print the data stored in the buffer
-            for (int i = 0; i < numberOfByteToRead; i++) {
+            for (int i = 0; i < numberOfBytesToRead; i++) {
                 std::cout << "Data[" << i << "]: " << static_cast<int>(data[i]) << std::endl;
             }
         } else {
             std::cout << "IMU NOT Detected. Error: " << i2c_flag << std::endl;
         }
 
-        wait_us(1000000); // Wait for 1 second
+        wait_us(1000000); 
     }
 }
