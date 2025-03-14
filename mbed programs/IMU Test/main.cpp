7e8437ceee4f640edc8e1541926d538ff3e34dd3
AbdallahCeesay <abdallah.ceesay@students.plymouth.ac.uk>
2025-03-14 22:40:05 +0000

    SerialStream works now but...
    Okay, I got the serial stream to work and I have adapted the BufferedSerial to a stream instance in the constructor of the BNO8x library.
    
    Now the issue I am having is that, whenever I compile the code, I get the following error mostly:
    1. Use of undeclared identifier  BNO080Async
    
    wait....what?!
    
    So I realized that the BNO08x library uses RTOS and not baremetal, therefore I removed the json file that contained the baremetal profile.
    
    Then I recompiled the flipping thing and it worked but....yes, there's always a but, but...nothing is been printed to the terminal.
    
    Now what I will do is, I will use the example code provided for the IMU and adapt the serial communication appropriately and see what happens

--- mbed programs/IMU Test/main.cpp
+++ mbed programs/IMU Test/main.cpp
@@ -5,19 +5,22 @@
 #define i2cadd 0x4a
 #define i2cportspeed 100000
 
-Serial debugport(USBTX, USBRX);
 
 PinName SDA = PB_9;
 PinName SCL = PB_8;
 PinName INTPin = PA_6;
 PinName RSTPin = PA_5;
 
-BNO080I2C imu(&debugport, SDA, SCL, INTPin, RSTPin, i2cadd, i2cportspeed);
 
 
 int main()
 {
+    BufferedSerial serial (USBTX, USBRX); // default baud rate set
+    SerialStream<BufferedSerial> debugport(serial); // adapts the new serial class(BufferedSerial) to a stream instance 
+
+    BNO080I2C imu(&debugport, SDA, SCL, INTPin, RSTPin, i2cadd, i2cportspeed);
+    
     //BNO080I2C imu(&debugport, SDA, SCL, INTPin, RSTPin, i2cadd, i2cportspeed);
-//    debugport.write("Hello World",11);
+    debugport.write("Hello World",11);
 
 }
