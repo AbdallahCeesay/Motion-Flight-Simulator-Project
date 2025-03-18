e7b854040c667480c097b4645aacaab7342cd66e
AbdallahCeesay <abdallah.ceesay@students.plymouth.ac.uk>
2025-03-18 16:48:08 +0000

    Reading ROLL, PITCH and YAW
    Modified the previous code to provide individual ROLL, PITCH, and YAW data.
    
    This works very well

--- mbed programs/IMU Test I2C/main.cpp
+++ mbed programs/IMU Test I2C/main.cpp
@@ -11,7 +11,7 @@ PinName SCL = PB_8;
 PinName INTPin = PA_6;
 PinName RSTPin = PA_5;
 
-BufferedSerial serial(USBTX, USBRX, 9600);
+BufferedSerial serial(USBTX, USBRX, 115200);
 SerialStream<BufferedSerial> debugport(serial);
 BNO080I2C imu(&debugport, SDA, SCL, INTPin, RSTPin, i2cadd, i2cportspeed);
 
@@ -26,7 +26,7 @@ int main() {
     else printf("Init Failed!\n");
 
     // Configure IMU reports
-    imu.enableReport(BNO080::ROTATION, 10);
+    imu.enableReport(BNO080::ROTATION, 20);
 
     while (true) {
         
@@ -35,9 +35,15 @@ int main() {
             if (imu.hasNewData(BNO080::ROTATION)) {
                 
                 TVector3 eulerDegrees = imu.rotationVector.euler() * (180.0 / M_PI);
-                debugport.printf("Rotation (deg): ");
-                eulerDegrees.print(debugport, true);
+                float roll = eulerDegrees[0];               //x-axis
+                float pitch = eulerDegrees[1];              //y-axis
+                float yaw = eulerDegrees[2];                //z-axis
+
+                debugport.printf("Roll: %.2f\n", roll);
+                debugport.printf("Pitch: %.2f\n", pitch);
+                debugport.printf("Yaw: %.2f\n", yaw);
                 debugport.printf("\n");
+         
             }
         }
     }
